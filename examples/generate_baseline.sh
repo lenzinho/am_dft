36922f9386417db5617121e6f3202ace22a8624f
lenzinho <lenzinho@lenzinhos-MacBook-Pro.local>
2016-05-29 09:18:06 -0700

    new baselines

---
+++ examples/generate_baseline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+odir=`pwd`
+
+./quick_make.sh sym
+./quick_make.sh tb
+
+dlist=`find . -maxdepth 1 -mindepth 1 -type d `
+for d in ${dlist}
+do
+	echo ${odir}/${d}
+	cd ${d}
+	sym -poscar outfile.POSCAR.supercell | tee baseline.sym
+	tb  -poscar outfile.POSCAR.supercell | tee baseline.tb
+	cd ${odir}
+done
