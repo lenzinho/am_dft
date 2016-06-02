9dc523125e41a456ec73d471d56e5867d6673632
lenzinho <lenzinho@lenzinhos-MacBook-Pro.local>
2016-06-01 21:57:03 -0700

    added matlab dispersion

--- matlab/master_tb.m
+++ matlab/master_tb.m
@@ -1,11 +1,9 @@
 clear;clc;
 tiny=1.0E-6;
-%
+
 [pg] = load_tb_point_group();
 [pp] = load_tb_matrix_elements();
-
-kpt = [0,0,0];
-[H] = get_tb_hamiltonian(pp,kpt);
+[H]  = get_tb_hamiltonian(pp,[0,0,0]);
 
 % check H = H'
 if (abs(norm(H-H'))>tiny)
@@ -22,3 +20,44 @@ for i = 1:pg.nsyms
     end
 end
 fprintf('ERROR: %i symmetry operations found to not commute with H.\n',j)
+
+
+%%
+
+clear;clc;
+tiny=1.0E-6;
+
+% kpoint path [frac]
+kstart=[
+    0.000000   0.000000   1.000000
+    0.500000   0.500000   1.000000
+    0.000000   0.000000   0.000000
+    ]';
+kend=[
+    0.000000   0.500000   0.500000
+    0.000000   0.000000   0.000000
+    0.000000   0.500000   0.000000
+    ]';
+
+[pg] = load_tb_point_group();
+[pp] = load_tb_matrix_elements();
+[pc] = load_poscar('outfile.POSCAR.primitive');
+[bz] = get_kpoint_path(pc.bas,kstart,kend,40);
+
+for i = 1:bz.npaths
+for j = 1:bz.ndivs
+    H = get_tb_hamiltonian(pp, bz.path(i).kpt_cart(:,j));
+    bz.path(i).D(:,j) = sort(real(eig(H)));
+end
+end
+
+for i = 1:bz.npaths
+    plot(bz.path(i).x,bz.path(i).D,'-')
+    hold on;
+end
+hold off;
+axis tight;
+box on;
+
+
+
