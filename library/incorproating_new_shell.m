bed73710b625044934dccbe5904a761299135985
lenzinho <lenzinho@lenzinhos-MacBook-Pro-2.local>
2017-02-22 19:48:49 -0800

    before copy

--- library/incorproating_new_shell.m
+++ library/incorproating_new_shell.m
@@ -19,20 +19,14 @@ tiny = am_lib.tiny;
 
 cutoff=5;
 fname='POSCAR';
-fname='POSCAR.BMg2';
+% fname='POSCAR.BMg2';
 % fname='outfile.supercell';
 flags='';
 
-[uc,~,ic] = get_cells(fname,flags);
+[uc,pc,~] = get_cells(fname,flags);
 
 
 
-
-% readjust cutoff based on unitcell
-cutoff = min([normc_(uc.bas)/2,cutoff]);
-
-% get conversion from primitive to supercell basis
-uc2pc = ic.bas/uc.bas; pc2uc=inv(uc2pc);
 
 % define function to apply symmetries to position vectors
 sym_apply_ = @(S,tau) reshape(matmul_(S(1:3,1:3,:),tau),3,[],size(S,3)) + S(1:3,4,:);
@@ -41,13 +35,21 @@ sym_apply_ = @(S,tau) reshape(matmul_(S(1:3,1:3,:),tau),3,[],size(S,3)) + S(1:3,
 sym_rebase_ = @(B,S) [[ matmul_(matmul_(B,S(1:3,1:3,:)),inv(B)), reshape(matmul_(B,S(1:3,4,:)),3,[],size(S,3))]; S(4,1:4,:)];
 
 % define function to get the clostest primitive lattice vector in supercell basis
-G_ = @(tau) tau - reshape(matmul_(uc2pc,mod_(matmul_(pc2uc,tau))),size(tau));
+G_ = @(uc2pc,tau) tau - reshape(matmul_(uc2pc,mod_(matmul_(inv(uc2pc),tau))),size(tau));
+
+
+
+% readjust cutoff based on unitcell
+cutoff = min([normc_(uc.bas)/2,cutoff]);
+
+% get conversion from primitive to supercell basis
+uc2pc = pc.bas/uc.bas;
 
 % define function to get bond vector from unit cell atom pair indicies
-diff_ = @(V) uc.tau(:,V(2,:))-uc.tau(:,V(1,:));
+diff_ = @(V) uc.tau(:,V(1,:))-uc.tau(:,V(2,:));
 
 % get point symmetries in [supercell basis]
-S = ic.S; nsyms = size(S,3); S = sym_rebase_(uc2pc,S);
+[S,~] = get_symmetries(pc); nSs = size(S,3); S = sym_rebase_(uc2pc,S);
 
 % determine primitive atoms shells which are connected
 P1s=[];P2s=[]; npcs=numel(uc.p2u); npairs=zeros(1,npcs);
@@ -59,66 +61,86 @@ for i = 1:npcs
     d=normc_(uc2ws(uc.bas*(uc.tau-Delta),uc.bas)); ex_=[d<cutoff]; npairs(i)=sum(ex_);
 
     % first atom in pair (must be in primitive cell); record G which shifts tau back to itself 
-    tau = sym_apply_(S,Delta); G=G_(tau); P1 = member_(mod_(tau-G),uc.tau);
-    
+    tau = sym_apply_(S,Delta); G=G_(uc2pc,tau); P1 = member_(mod_(tau-G),uc.tau);
+
     % second atom in pair (SEE NOTEs)
-    tau = sym_apply_(S,uc.tau(:,ex_));    P2 = member_(mod_(tau-G),uc.tau);
+    tau = sym_apply_(S,uc.tau(:,ex_));          P2 = member_(mod_(tau-G),uc.tau);
 
     % save results
     P1s = [P1s;repelem(P1,npairs(i),1)]; P2s = [P2s;P2]; %#ok<AGROW>
 end
 
-% define function to simultaneously apply operation (helps to not forget about one)
-bundle_ = @(ex_,A,B) deal(A(:,ex_),B(ex_));
-
-% pair vector and indicies
-V = [ P1s(:), P2s(:) ].'; Vinds = [1:(sum(npairs)*nsyms)];
-
-% check that all atoms are mapped
-if any(V(:)==0,1); error('something is wrong; space symmetry must map atoms onto atoms without exceptions!'); end
-
 % create a unique pair label
-[~,i2p,p2i]=unique(sort(V).','rows','stable');
+V=[P1s(:),P2s(:)].'; [~,i2p,p2i]=unique(sort(V).','rows','stable'); V=V(:,i2p);
 
-% get permutation representation
-PM = zeros(sum(npairs),nsyms); PM(Vinds) = p2i;
-% CHECK: PM(all(diff(PM,1,2)==0,2),:), V(:,PM(all(diff(PM,1,2)==0,2),1))
+% get permutation representation (entries are unique pair indicies)
+PM = reshape(p2i,[sum(npairs),nSs]);
 
 % get map connecting pairs (sort A based on distances)
-A = get_binary_rep(PM); A=A( rankcol_( normc_(uc2ws(uc.bas*diff_( V(:,i2p(findrow_(A))) ),uc.bas)) ), :);
-% CHECK: spy(A(:,~all(A==0,1)))
-% CHECK: diff_ = @(V) uc.tau(:,V(2,:))-uc.tau(:,V(1,:));
-% CHECK: normc_(uc2ws(uc.bas*diff_( V(:,i2p(A(30,:))) ),uc.bas))
+A = get_binary_rep(PM); 
+
+% sort rows of A based on pair distances
+d_ = @(r) normc_(uc2ws(uc.bas*r,uc.bas)); 
+A = A( rankcol_( d_(diff_(V(:,findrow_(A)))) ),:);
+
+% find the symmetry corresponding to the identity
+identity = member_(reshape(eye(4),16,1),reshape(S,16,[]));
+
+% bundle
+bundle_ = @(ex_,A,B) deal(A(:,ex_),B(ex_));
 
 Z=[];
-for i = 1:ic.natoms
+for i = 1:pc.natoms
+    pc_id = pc.p2u(i);
+    
     Y=[];
-    for p2i = 1:size(A,1)
-        [U,Uinds] = bundle_(i2p(A(p2i,:)),V,Vinds); ic_id = ic.i2u(i); ex_ = any(U==ic_id,1);
+    for irres = 1:size(A,1)
+        % get all connected shells which involve irreducible atom i
+        [U,Uinds]=bundle_(A(irres,:),V,[1:size(V,2)]); ex_=any(U==pc_id,1);  
+        
+        % if such a shell exists, analyze it
         if any(ex_)
-           rep = find(ex_,1);  % an arbitrary pair in the orbit
-            xy = U(:,rep);     % uc indicies
-            mn = uc.u2p(xy).'; % pc indicies
-            ij = uc.u2i(xy).'; % ic indicies
-            r  =  diff_(xy); r = uc2ws(uc.bas*r,uc.bas); % bond vector
-            d  =   normc_(r); % bond length
-            w  =    sum(ex_); % number of pairs in orbit
+            % trim
+            U=U(:,ex_);
+            
+            % choose the rep as the bond involving the greatest number of primitive cell atoms 
+            [~,rep] = max(sum(ismember(U,pc.p2u),1)); 
+            
+            % see if it is flipped
+            if U(1,rep)==pc_id; isflipped=+1; else; isflipped=-1; end
+            
+            % sort indicies to put pc_id at "center"
+            U(U==pc_id)=0; U=sort(U); U(U==0)=pc_id;
 
+            % record basic things
+            xy  = U(:,rep);     % uc indicies
+            mn  = uc.u2p(xy).'; % pc indicies
+            ij  = uc.u2i(xy).'; % ic indicies
+            v   = diff_(xy); v = uc2ws(uc.bas*v,uc.bas); % bond vector
+            d   = normc_(v);    % bond length
+            w   = sum(ex_);     % number of pairs in orbit
 
-    %             s_ck = PM(find(A(2,:),1),:)== ;
-    %  PM(find(A(2,:),1),:) == PM(find(A(p2i,:),1),24)
-            if (ij(1)~=ic.u2i(ic_id))
-                Y = [Y,[d(:);-r(:);w(:);ij([2,1]);mn([2,1]);-p2i]];
-            else
-                Y = [Y,[d(:);+r(:);w(:);ij([1,2]);mn([1,2]);+p2i]];
-            end
+            % record stabilizer and generators
+            row = find(PM(:,identity)==Uinds(rep),1);
+            [~,b,c] = unique( PM(row,:) ) ;
+            s_ck = [c(:).'==c(identity)];
+            g_ck = false(nSs,1); g_ck(b(:)) = true;
+
+            % rep = Uinds(find(ex_,1));
+            % row = find(PM(:,identity)==rep,1);
+            % x = mod_(diff_(V(:,PM(row,:))));
+            % y = mod_(reshape(sym_apply_(S,diff_(V(:,rep))),3,[]));
+            % max(abs(x(:)-y(:))) % should be equal to zero (and is)
+            
+            % [ d(1), r(2,3,4), w(5), ij(6,7), mn(8,9), irres(10), s_ck, g_ck ]
+            Y = [Y,[d(:);+v(:);w(:);ij([1,2]);mn([1,2]);+irres*isflipped;s_ck(:);g_ck(:)]];
         end
     end
 
     bar_ = @(x) repmat('-',[1,x]);
-    fprintf('irreducible atom %i at [%.5f,%.5f,%.5f] has %i shells\n', i,ic.bas*ic.tau(:,i),size(Y,2));
-    fprintf('%-10s   %-30s   %-4s   %-7s   %-7s   %-4s\n', 'd [cart]','bond [cart]','#','ic(i,j)','pc(m,n)','flp?'); 
-    fprintf('%-10s   %-30s   %-4s   %-7s   %-7s   %-4s\n', bar_(10),bar_(30),bar_(4),bar_(7),bar_(7),bar_(4));
+    fprintf('primitive atom %i at [%.5f,%.5f,%.5f] has %i shells\n', i,pc.bas*pc.tau(:,i),size(Y,2));
+    fprintf('%-10s    %-30s   %-4s   %-7s   %-7s   %-4s\n', 'd [cart]','bond [cart]','#','ic(i,j)','pc(m,n)','flp?'); 
+    fprintf('%-10s    %-30s   %-4s   %-7s   %-7s   %-4s\n', bar_(10),bar_(30),bar_(4),bar_(7),bar_(7),bar_(4));
     fprintf('%10.5f  %10.5f %10.5f %10.5f   %4i   %-3i-%3i   %-3i-%3i   %3i\n', Y(1:10,:) );
     fprintf('\n');
 
@@ -126,33 +148,28 @@ for i = 1:ic.natoms
 end
 
 % get irreducible shells (a negative p2i means the pair was flipped)
-p2i = Z(end,:); [~,i2p,~]=unique(abs(p2i),'stable');
+p2i = Z(10,:); [~,i2p,~]=unique(abs(p2i),'stable');
 
+% save structures
+v_ = @(Z) Z(2:4,:); w_ = @(Z) Z(5,:); i_ = @(Z) Z(6,:); j_ = @(Z) Z(7,:); m_ = @(Z) Z(8,:); n_ = @(Z) Z(9,:); 
+s_ck_ = @(Z) Z(10+[1:nSs],:); g_ck_ = @(Z) Z(10+nSs+[1:nSs],:);
+
+pair_ = @(uc,S,Z) struct('units','frac','bas',uc.bas,'recbas',uc.recbas, ...
+    'symb',{{uc.symb{:}}},'species',uc.species,'mass',uc.mass,...
+    'nshells',size(Z,2),'norbits',w_(Z),'v',v_(Z),'i',i_(Z),'j',j_(Z),'m',m_(Z),'n',n_(Z), ...
+    's_ck',s_ck_(Z),'g_ck',g_ck_(Z),'nRs',size(S,3),'S',S);
+
+pp = pair_(uc,S,Z);
+ip = pair_(uc,S,Z(:,i2p(rankcol_( Z([1],i2p)))));
+
+% print irreducible pairs
 bar_ = @(x) repmat('-',[1,x]);
 fprintf('%i irreducible shells\n',numel(i2p));
-fprintf('%-10s   %-30s   %-4s   %-7s   %-7s   %-4s\n', 'd [cart]','bond [cart]','#','ic(i,j)','pc(m,n)','flp?'); 
-fprintf('%-10s   %-30s   %-4s   %-7s   %-7s   %-4s\n', bar_(10),bar_(30),bar_(4),bar_(7),bar_(7),bar_(4));
+fprintf('%-10s    %-30s   %-4s   %-7s   %-7s   %-4s\n', 'd [cart]','bond [cart]','#','ic(i,j)','pc(m,n)','flp?'); 
+fprintf('%-10s    %-30s   %-4s   %-7s   %-7s   %-4s\n', bar_(10),bar_(30),bar_(4),bar_(7),bar_(7),bar_(4));
 fprintf('%10.5f  %10.5f %10.5f %10.5f   %4i   %-3i-%3i   %-3i-%3i   %3i\n', Z(1:10,i2p(rankcol_( Z([1],i2p)))) );
 fprintf('\n');
 
-
-
-
-
-% pairs = V(:,i2p(findrow_(A)));
-% r = uc2ws(uc.bas*diff_(pairs),uc.bas);
-% d = normc_(r);
-% Z = [d;r;sum(A,2).';uc.u2i(pairs);uc.u2p(pairs);1:numel(d)];
-% Z = Z(:, rankcol_([d;r]) );
-% Z = Z(:, i2p(rankcol_( Z([8,1],i2p))) ) );
-
-
-% V(:,i2p(A(5,:)))
-% fprintf(' %10f %10f %10f %10f %3i %3i %3i %3i %3i %3i \n', Z)
-
-
-
-% pairs = uc.u2p(V(:,i2p(A(6,:))))
 
 
 
