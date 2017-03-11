bda159223b53e39b18faa2d8d250642d0083f974
lenzinho <lenzinho@lenzinhos-MacBook-Pro-2.local>
2017-03-10 20:35:53 -0800

    broken

--- library/incorproating_new_shell.m
+++ library/incorproating_new_shell.m
@@ -4,23 +4,11 @@
 %   3) save [g_ck] the space symmetries which generate the orbit
 %   4) save [Qi]   the space symmetry   which maps the prototypical
 %                  irreducible pair onto the rep [v]
-%
-% NOTE: requires p2u to point to atoms who's uc coordinates share a common
-% closest primitive lattice point. set this in get_primitive_cell
-% 
-% NOTE: application of space symmerties requires the permutation matrices
-% P1s and P2s to not have any zero values. all atoms must be mapped onto
-% another atom.
-%
-% NOTE: The primitive shells as defined here involve only two primitive
-% cell atoms for each given orbit. Although some space symmetries may map
-% atoms onto other primitive cell atoms, those orbits are decomposed into
-% two unique primitive shells which are related to the prototypical
-% irreducible shell via Qi.
 
 
 
-clear;clc
+
+clear all;clc
 
 import am_lib.*
 
@@ -28,31 +16,50 @@ import am_lib.*
 
 tiny = am_lib.tiny;
 
-cutoff=5;
+cutoff=3;
 fname='POSCAR';
 % fname='POSCAR.BMg2';
 % fname='outfile.supercell';
-% fname='infile.supercell';
+fname='infile.supercell';
 flags='';
 
 % get cells
 [uc,pc,ic] = get_cells(fname,flags);
 
 % get irreducible shells
-[pp]         = get_pairs(pc,uc,cutoff)
+[bvk,pp] = get_bvk(cutoff,pc,uc,'infile.force_position.4.00-300.short',flags);
 
-% force constant model
-% bvk = get_bvk_model(pc,pp);
+% %% numerical
+bzp = get_bz_path(pc,'fcc');
+plot_bvk_dispersion(bvk,bzp)
 
-% [bvk,pp] = get_bvk(cutoff,pc,uc,'infile.force_position.4.00-300',flags);
-
-% bzp = get_bz_path(pc,'fcc');
-
-% plot_bvk_dispersion(bvk,bzp);
 %%
-plot_bvk_dispersion(bvk,bzp,pp,uc) % MATCHES SYMBOLIC VERSION
 
+clear;clc; import am_lib.*
 
+cutoff=2.5;
+fname='POSCAR';
+% fname='POSCAR.BMg2';
+% fname='outfile.supercell';
+fname='infile.supercell';
+flags='';
+
+% get cells
+[uc,pc,ic] = get_cells(fname,flags);
+
+% get tight-binding
+spdf={'d','p'}; nskips=5; fname='EIGENVAL.ibz'; Ef=0;
+[tb,pp] = get_tb(pc,uc,cutoff,spdf,nskips,Ef,fname,flags)
+
+% plot electron band structure along path
+bzp = get_tb_dispersion(tb,bzp);
+
+% define figure properties
+fig_ = @(h)       set(h,'color','white');
+axs_ = @(h,qt,ql) set(h,'Box','on','XTick',qt,'Xticklabel',ql);
+
+figure(1); fig_(gcf); plot(bzp.x,sort(real(bzp.E)),'-k');
+axs_(gca,bzp.qt,bzp.ql); axis tight; ylabel('Energy E'); xlabel('Wavevector k');
 
 
 
