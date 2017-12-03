d0affc16e423820ca0096c5877bf0249881e581a
lenzinho <lenzinho@lenzinhos-MacBook-Pro-2.local>
2017-12-03 10:52:55 -0500

    cleanup

--- _old/examples/custom/quick_make.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ODIR=$PWD
-cd /Volumes/Lenzinho/MyLinux/calc.vasp.5.3.3/development/am_lib/
-make $1
-cd ${ODIR}
-
--- _old/matlab/tb_model_fitter_compute_delta_vector.m
+++
@@ -1,6 +0,0 @@
-function [d,F1,A,B] = tb_model_fitter_compute_delta_vector(bz,dr,pg,x,mask)
-    [J,F1,~] = tb_model_fitter_compute_jacobian(bz,dr,pg,x,mask);
-    A = transpose(J)*J;
-    B = transpose(J)*F1;
-    d(:,1) = A\B;
-end
